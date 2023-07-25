ca84a8ebc8daaa01ba0cb6fec701f2f1a5a73aee
dvsilva <danyllo.dvs@gmail.com>
2023-07-25 17:25:41 -0300

    add gitignore

---
+++ src/main/java/br/com/dvsilva/aula3/Aluno.java
@@ -0,0 +1,15 @@
+package br.com.dvsilva.aula3;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class Aluno {
+
+    @Getter
+    @Setter
+    private String nome;
+
+    @Getter
+    private int idade;
+
+}
---
+++ src/main/java/br/com/dvsilva/aula3/Aula3.java
@@ -0,0 +1,11 @@
+package br.com.dvsilva.aula3;
+
+public class Aula3 {
+
+    public static void main(String[] args) {
+        Aluno aluno = new Aluno();
+
+
+    }
+
+}
