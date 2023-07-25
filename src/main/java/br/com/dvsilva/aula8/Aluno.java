4eb3dc00e57cbeb9abb089effcd161c133e579dc
dvsilva <danyllo.dvs@gmail.com>
2023-07-25 20:36:15 -0300

    Aula 8 - @Value

---
+++ src/main/java/br/com/dvsilva/aula8/Aluno.java
@@ -0,0 +1,67 @@
+package br.com.dvsilva.aula8;
+
+import lombok.Data;
+import lombok.NonNull;
+import lombok.ToString;
+import lombok.Value;
+import lombok.With;
+import lombok.experimental.NonFinal;
+
+@Value
+//@Data
+//@EqualsAndHashCode(
+////    exclude = {"nota"}
+//    onlyExplicitlyIncluded = true
+//)
+@ToString(
+        includeFieldNames = true,
+//        exclude = {"sexo", "nota"},
+//        of = {"idade", "nome", "ativo"},
+//        callSuper = true,
+        doNotUseGetters = false,
+        onlyExplicitlyIncluded = true
+)
+//@Getter
+//@Setter
+public class Aluno {
+
+    @NonFinal
+    @With
+    @NonNull
+//    @EqualsAndHashCode.Include
+    @ToString.Include
+//    @Getter(AccessLevel.PUBLIC)
+//    @Setter(AccessLevel.PACKAGE)
+    private String nome;
+
+//    @Getter(AccessLevel.PUBLIC)
+//    @Setter(AccessLevel.PACKAGE)
+    private Double nota;
+
+//    @Getter(AccessLevel.PUBLIC)
+//    @Setter(AccessLevel.PACKAGE)
+    private char sexo;
+
+    @ToString.Include
+//    @Getter(AccessLevel.PUBLIC)
+//    @Setter(AccessLevel.PACKAGE)
+    private int idade;
+
+    @ToString.Include
+//    @Getter(AccessLevel.PUBLIC)
+//    @Setter(AccessLevel.PACKAGE)
+    private boolean ativo;
+
+    public Aluno(@NonNull String nome, Double nota, char sexo, int idade, boolean ativo) {
+        this.nome = nome;
+        this.nota = nota;
+        this.sexo = sexo;
+        this.idade = idade;
+        this.ativo = ativo;
+    }
+
+    public String getNome(){
+        return "nome formatado " + nome;
+    }
+
+}
---
+++ src/main/java/br/com/dvsilva/aula8/Aula8.java
@@ -0,0 +1,42 @@
+package br.com.dvsilva.aula8;
+
+public class Aula8 {
+
+    public static void main(String[] args) {
+        Aluno aluno = new Aluno(
+                "novo aluno", 1.0, 'm',
+                19, true
+        );
+        aluno = aluno.withNome("xxx");
+//        aluno.setNome("novo aluno");
+//        aluno.setNota(8.5);
+//        aluno.setSexo('M');
+//        aluno.setIdade(18);
+//        aluno.setAtivo(Boolean.TRUE);
+
+        System.out.println(aluno.getNome() + " - " +
+                aluno.getIdade());
+        System.out.println(aluno.toString());
+
+
+        Aluno aluno2 = new Aluno(
+                "novo aluno 2", 1.0, 'm', 19,
+                true
+        );
+//        aluno.setNome("novo aluno 2");
+//        aluno.setNota(8.0);
+//        aluno.setSexo('M');
+//        aluno.setIdade(12);
+//        aluno.setAtivo(Boolean.TRUE);
+
+//        aluno2.setNome("novo aluno");
+//        aluno2.setNota(8.5);
+//        aluno2.setSexo('M');
+//        aluno2.setIdade(18);
+//        aluno2.setAtivo(Boolean.TRUE);
+
+        System.out.println(aluno.equals(aluno2));
+        System.out.println(aluno == aluno2);
+    }
+
+}
