fb2da27f62d7e8947083d1330d79fc3b998a60d2
dvsilva <danyllo.dvs@gmail.com>
2023-07-25 17:18:55 -0300

    Aula 2 - Iniciando Projeto

---
+++ src/main/java/br/com/dvsilva/aula2/Aula2.java
@@ -0,0 +1,14 @@
+package br.com.dvsilva.aula2;
+
+public class Aula2 {
+
+    public static void main(String[] args) {
+
+        Aluno aluno = new Aluno();
+        aluno.getNome();
+        aluno.getIdade();
+//        System.out.println();
+
+    }
+
+}
