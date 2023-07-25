b0e34238bed9bffd3f38ba268da8ba7e0ef54948
dvsilva <danyllo.dvs@gmail.com>
2023-07-25 17:34:32 -0300

    Aula 3 - @Getter e @Setter

--- src/main/java/br/com/dvsilva/aula3/Aluno.java
+++ src/main/java/br/com/dvsilva/aula3/Aluno.java
@@ -1,15 +1,19 @@
 package br.com.dvsilva.aula3;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 
+@Getter
+@Setter
 public class Aluno {
 
-    @Getter
-    @Setter
+    @Getter(AccessLevel.PUBLIC)
+    @Setter(AccessLevel.PACKAGE)
     private String nome;
 
-    @Getter
+    @Getter(AccessLevel.PUBLIC)
+    @Setter(AccessLevel.PACKAGE)
     private int idade;
 
 }
--- src/main/java/br/com/dvsilva/aula3/Aula3.java
+++ src/main/java/br/com/dvsilva/aula3/Aula3.java
@@ -4,6 +4,12 @@ public class Aula3 {
 
     public static void main(String[] args) {
         Aluno aluno = new Aluno();
+        aluno.setIdade(18);
+        aluno.setNome("novo aluno");
+
+        System.out.println(aluno.getNome() + " - " +
+                aluno.getIdade());
+
 
 
     }
