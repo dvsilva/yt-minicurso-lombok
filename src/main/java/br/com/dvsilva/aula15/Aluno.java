c06a327358d08802ceb002e2fe0fd97cc4455fad
dvsilva <danyllo.dvs@gmail.com>
2023-07-25 21:55:34 -0300

    Aula 15 - @SneakyThrows

---
+++ src/main/java/br/com/dvsilva/aula15/Aluno.java
@@ -0,0 +1,95 @@
+package br.com.dvsilva.aula15;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Singular;
+import lombok.Synchronized;
+import lombok.ToString;
+import lombok.With;
+import lombok.experimental.NonFinal;
+
+import java.util.List;
+
+@Builder
+//@Builder(builderClassName = "HelloWorldBuilder",
+//        buildMethodName = "execute",
+//        builderMethodName = "helloWorld",
+//        toBuilder = true,
+//        access = AccessLevel.PUBLIC,
+//        setterPrefix = "set"
+//)
+
+//@Value
+@Data
+//@EqualsAndHashCode(
+////    exclude = {"nota"}
+//    onlyExplicitlyIncluded = true
+//)
+@ToString(
+        includeFieldNames = true,
+//        exclude = {"sexo", "nota"},
+//        of = {"idade", "nome", "ativo"},
+//        callSuper = true,
+        doNotUseGetters = false,
+        onlyExplicitlyIncluded = true
+)
+//@Getter
+//@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class Aluno {
+//    private final Object readLock = new Object();
+
+    @NonFinal
+    @With
+    @NonNull
+//    @EqualsAndHashCode.Include
+    @ToString.Include
+//    @Getter(AccessLevel.PUBLIC)
+//    @Setter(AccessLevel.PACKAGE)
+    private String nome;
+
+//    @Getter(AccessLevel.PUBLIC)
+//    @Setter(AccessLevel.PACKAGE)
+
+    @With
+    private Double nota;
+
+//    @Getter(AccessLevel.PUBLIC)
+//    @Setter(AccessLevel.PACKAGE)
+
+    @With
+    private char sexo;
+
+    @With
+    @ToString.Include
+//    @Getter(AccessLevel.PUBLIC)
+//    @Setter(AccessLevel.PACKAGE)
+    private int idade;
+
+    @Builder.Default
+    @With
+    @ToString.Include
+//    @Getter(AccessLevel.PUBLIC)
+//    @Setter(AccessLevel.PACKAGE)
+    private boolean ativo = Boolean.FALSE;
+
+    @Singular(value = "addInteresse")
+    @With
+    @ToString.Include
+    private List<String> interesses;
+
+    public String getNome(){
+        return "nome formatado " + nome;
+    }
+
+    @Synchronized //("readLock")
+    public void helloWorld() {
+        System.out.println("world");
+    }
+}
---
+++ src/main/java/br/com/dvsilva/aula15/Aula15.java
@@ -0,0 +1,111 @@
+package br.com.dvsilva.aula15;
+
+import lombok.Cleanup;
+import lombok.SneakyThrows;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import static java.util.Collections.emptyList;
+
+public class Aula15 {
+
+    public static void main(String[] args) {
+        lerEEscreverArquivo();
+
+        Aluno aluno = new Aluno(
+                "novo aluno", 1.0, 'm',
+                19, true, emptyList()
+        );
+        aluno = aluno.withNome("xxx");
+//        aluno.setNome("novo aluno");
+//        aluno.setNota(8.5);
+//        aluno.setSexo('M');
+//        aluno.setIdade(18);
+//        aluno.setAtivo(Boolean.TRUE);
+
+        System.out.println(aluno.getNome() + " - " +
+                aluno.getIdade());
+        System.out.println(aluno.toString());
+
+
+        Aluno aluno2 = new Aluno(
+                "novo aluno 2", 1.0, 'm', 19,
+                true, emptyList()
+        );
+//        aluno.setNome("novo aluno 2");
+//        aluno.setNota(8.0);
+//        aluno.setSexo('M');
+//        aluno.setIdade(12);
+//        aluno.setAtivo(Boolean.TRUE);
+
+//        aluno2.setNome("novo aluno");
+//        aluno2.setNota(8.5);
+//        aluno2.setSexo('M');
+//        aluno2.setIdade(18);
+//        aluno2.setAtivo(Boolean.TRUE);
+
+        System.out.println(aluno.equals(aluno2));
+        System.out.println(aluno == aluno2);
+
+        Aluno alunoWith = new Aluno()
+                .withNome("nome")
+                .withIdade(20)
+                .withNota(4.2)
+                .withSexo('M')
+                .withInteresses(emptyList())
+                .withAtivo(true);
+
+        alunoWith = alunoWith.withNome("novo nome de aluno");
+
+        System.out.println(alunoWith);
+
+//        Aluno alunoBuilder = Aluno.helloWorld()
+//                .setNome("aluno builder")
+//                .setIdade(20)
+//                .setNota(4.2)
+//                .setSexo('M')
+//                .execute();
+
+        Aluno alunoBuilder = Aluno.builder()
+                .nome("aluno builder")
+                .idade(20)
+                .nota(4.2)
+                .sexo('M')
+                .addInteresse("Xadrez")
+                .addInteresse("Dominó")
+//                .interesses(new ArrayList<>())
+//                .ativo(true)
+                .build();
+
+//        alunoBuilder.getInteresses().add("Futebol");
+        System.out.println(alunoBuilder);
+    }
+
+    @SneakyThrows(value = {
+            FileNotFoundException.class,
+            IOException.class
+    })
+    private static void lerEEscreverArquivo() {
+        @Cleanup InputStream in = new FileInputStream(
+                "src//main//resources//arquivo.txt");
+        @Cleanup OutputStream out = new FileOutputStream(
+                "src//main//resources//resultado.txt");
+
+        byte[] b = new byte[10000];
+
+        while (true) {
+            int r = in.read(b);
+            if (r == -1) break;
+            out.write(b, 0, r);
+        }
+
+//        in.close();
+//        out.close();
+    }
+
+}
