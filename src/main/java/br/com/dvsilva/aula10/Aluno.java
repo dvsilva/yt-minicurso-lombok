18e397641c4a70fe0c67e486c9dfa101e6080d9c
dvsilva <danyllo.dvs@gmail.com>
2023-07-25 21:08:55 -0300

    Aula 10 - Anotações de Construtores

---
+++ src/main/java/br/com/dvsilva/aula10/Aluno.java
@@ -0,0 +1,70 @@
+package br.com.dvsilva.aula10;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import lombok.With;
+import lombok.experimental.NonFinal;
+
+//@Value
+@Data
+//@EqualsAndHashCode(
+////    exclude = {"nota"}
+//    onlyExplicitlyIncluded = true
+//)
+@ToString(
+        includeFieldNames = true,
+//        exclude = {"sexo", "nota"},
+//        of = {"idade", "nome", "ativo"},
+//        callSuper = true,
+        doNotUseGetters = false,
+        onlyExplicitlyIncluded = true
+)
+//@Getter
+//@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class Aluno {
+
+    @NonFinal
+    @With
+    @NonNull
+//    @EqualsAndHashCode.Include
+    @ToString.Include
+//    @Getter(AccessLevel.PUBLIC)
+//    @Setter(AccessLevel.PACKAGE)
+    private String nome;
+
+//    @Getter(AccessLevel.PUBLIC)
+//    @Setter(AccessLevel.PACKAGE)
+
+    @With
+    private Double nota;
+
+//    @Getter(AccessLevel.PUBLIC)
+//    @Setter(AccessLevel.PACKAGE)
+
+    @With
+    private char sexo;
+
+    @With
+    @ToString.Include
+//    @Getter(AccessLevel.PUBLIC)
+//    @Setter(AccessLevel.PACKAGE)
+    private int idade;
+
+    @With
+    @ToString.Include
+//    @Getter(AccessLevel.PUBLIC)
+//    @Setter(AccessLevel.PACKAGE)
+    private boolean ativo;
+
+    public String getNome(){
+        return "nome formatado " + nome;
+    }
+
+}
